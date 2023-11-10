08c9d8b45beb6e0e127cd9cd02343de3040686ba
Paulo <deoliveira.pe@gmail.com>
2023-11-09 22:00:21 -0300

    config: project structure

---
+++ authmicroservice/src/main/java/com/clubeevantagens/authmicroservice/CluBeeVantagensAuthMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.clubeevantagens.authmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CluBeeVantagensAuthMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CluBeeVantagensAuthMicroserviceApplication.class, args);
+	}
+
+}
---
+++ authmicroservice/src/test/java/com/clubeevantagens/authmicroservice/CluBeeVantagensAuthMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clubeevantagens.authmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CluBeeVantagensAuthMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
