4c87d90a36df709de4fb24becbdcd312e1ea4a65
Alessandro <alessandropds55@gmail.com>
2024-05-13 20:57:10 -0300

    refact: Alteração do CORS para "http://localhost:5173" usado pelo time de Front

--- authmicroservice/src/main/java/com/clubeevantagens/authmicroservice/CluBeeVantagensAuthMicroserviceApplication.java
+++ authmicroservice/src/main/java/com/clubeevantagens/authmicroservice/CluBeeVantagensAuthMicroserviceApplication.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -21,5 +22,18 @@ public class CluBeeVantagensAuthMicroserviceApplication implements WebMvcConfigu
 
 		System.out.println("BACKEND ESTA LIGADO");
 	}
+
+	// CrossOrigin global
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+
+		registry.addMapping("/**")
+				.allowedMethods("*")
+				.allowedHeaders("*")
+				.allowedOrigins("http://localhost:5173");
+
+
+	}
+
 
 }
