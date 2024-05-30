a431786c047930108e39f1dee9a63c5a44b9df14
Alessandro <alessandropds55@gmail.com>
2024-05-30 19:35:35 -0300

    feat: Adicionando e implementando UserService

--- authmicroservice/src/main/java/com/clubeevantagens/authmicroservice/controller/UserController.java
+++ authmicroservice/src/main/java/com/clubeevantagens/authmicroservice/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.clubeevantagens.authmicroservice.controller;
 import com.clubeevantagens.authmicroservice.model.User;
 import com.clubeevantagens.authmicroservice.repository.UserRepository;
+import com.clubeevantagens.authmicroservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,140 +14,34 @@ import java.util.Optional;
 @RequestMapping("/api/users")
 public class UserController {
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
     // CREATE
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody User user) {
-
-
-
-        // Verifica se o email já está cadastrado
-        if (userRepository.findUserByEmail(user.getEmail()).isPresent()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Email já cadastrado");
-        }
-
-        // Valida CPF se não for nulo
-        if (user.getCpf() != null && !isValidCPF(user.getCpf())) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("CPF inválido");
-        }
-
-        // Valida CNPJ se não for nulo
-        if (user.getCnpj() != null && !isValidCNPJ(user.getCnpj())) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("CNPJ inválido");
-        }
-
-        // Salva o usuário no banco de dados
-        userRepository.save(user);
-        return ResponseEntity.status(HttpStatus.CREATED).body("Usuario salvo com sucesso");
+        return userService.registerUser(user);
     }
 
 
     // READ
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers() {
-        List<User> users = userRepository.findAll();
-        return ResponseEntity.ok(users);
+        return userService.getAllUsers();
     }
 
 
     // UPDATE
     @PutMapping("/{id}")
     public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User newUser) {
-        Optional<User> userOptional = userRepository.findById(id);
-        if (userOptional.isPresent()) {
-            User user = userOptional.get();
-            user.setName(newUser.getName());
-            user.setEmail(newUser.getEmail());
-            user.setPassword(newUser.getPassword());
-            user.setTermsOfUse(newUser.isTermsOfUse());
-            user.setDateTermsOfUse(newUser.getDateTermsOfUse());
-            user.setCpf(newUser.getCpf());
-            user.setCnpj(newUser.getCnpj());
-
-            userRepository.save(user);
-            return ResponseEntity.ok(user);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+       return userService.updateUser(id,newUser);
     }
 
     // DELETE
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteUser(@PathVariable Long id) {
-        Optional<User> userOptional = userRepository.findById(id);
-        if (userOptional.isPresent()) {
-            userRepository.deleteById(id);
-            return ResponseEntity.ok().build();
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        return userService.deleteUser(id);
     }
 
-
-    // Remove Formatação de CPF ou CNPJ
-    public String removeFormatting(String value) {
-        return value.replaceAll("[^\\d]", ""); // Remove todos os caracteres que não são dígitos
-    }
-
-
-    // Validador de CPF
-    public boolean isValidCPF(String cpf) {
-
-        cpf = removeFormatting(cpf); // Remove formatação
-
-        if (cpf == null || cpf.length() != 11 || cpf.matches("(\\d)\\1{10}")) return false;
-
-        try {
-            int sum = 0;
-            for (int i = 0; i < 9; i++) {
-                sum += Character.getNumericValue(cpf.charAt(i)) * (10 - i);
-            }
-            int firstCheckDigit = 11 - (sum % 11);
-            if (firstCheckDigit > 9) firstCheckDigit = 0;
-            if (firstCheckDigit != Character.getNumericValue(cpf.charAt(9))) return false;
-
-            sum = 0;
-            for (int i = 0; i < 10; i++) {
-                sum += Character.getNumericValue(cpf.charAt(i)) * (11 - i);
-            }
-            int secondCheckDigit = 11 - (sum % 11);
-            if (secondCheckDigit > 9) secondCheckDigit = 0;
-            return secondCheckDigit == Character.getNumericValue(cpf.charAt(10));
-        } catch (Exception e) {
-            return false;
-        }
-    }
-
-
-    // Validador de CNPJ
-    public boolean isValidCNPJ(String cnpj) {
-
-        cnpj = removeFormatting(cnpj); // Remove formatação
-
-        if (cnpj == null || cnpj.length() != 14 || cnpj.matches("(\\d)\\1{13}")) return false;
-
-        try {
-            int[] weights = {6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
-            int sum = 0;
-            for (int i = 0; i < 12; i++) {
-                sum += Character.getNumericValue(cnpj.charAt(i)) * weights[i + 1];
-            }
-            int firstCheckDigit = 11 - (sum % 11);
-            if (firstCheckDigit > 9) firstCheckDigit = 0;
-            if (firstCheckDigit != Character.getNumericValue(cnpj.charAt(12))) return false;
-
-            sum = 0;
-            for (int i = 0; i < 13; i++) {
-                sum += Character.getNumericValue(cnpj.charAt(i)) * weights[i];
-            }
-            int secondCheckDigit = 11 - (sum % 11);
-            if (secondCheckDigit > 9) secondCheckDigit = 0;
-            return secondCheckDigit == Character.getNumericValue(cnpj.charAt(13));
-        } catch (Exception e) {
-            return false;
-        }
-    }
 
 
 
