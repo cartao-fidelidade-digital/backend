f743831986f08319efae38935ad927cc0660b018
Alessandro <alessandropds55@gmail.com>
2024-06-02 20:53:18 -0300

    config: Insera Roles(Client e Company) ao iniciar o Spring

--- authmicroservice/src/main/java/com/clubeevantagens/authmicroservice/model/Role.java
+++ authmicroservice/src/main/java/com/clubeevantagens/authmicroservice/model/Role.java
@@ -3,6 +3,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.boot.context.properties.bind.ConstructorBinding;
 
 import java.util.List;
 
@@ -11,6 +12,8 @@ import java.util.List;
 @Setter
 @ToString
 @EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Role {
     private static final long serialVersionUID = 1L;
