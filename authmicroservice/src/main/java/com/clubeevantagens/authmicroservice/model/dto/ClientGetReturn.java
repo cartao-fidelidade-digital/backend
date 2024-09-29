9a7785bb70781c70583c2c3fdaf4db31e5af5fc9
Alessandro Pereira <alessandropds55@gmail.com>
2024-09-29 15:05:35 -0300

    docs: altera retorno de "Client" no Swagger

--- authmicroservice/src/main/java/com/clubeevantagens/authmicroservice/model/dto/ClientGetReturn.java
+++ authmicroservice/src/main/java/com/clubeevantagens/authmicroservice/model/dto/ClientGetReturn.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @ToString
@@ -16,5 +18,11 @@ public class ClientGetReturn {
     private String phoneNumber;
     private String socialName;
     private String preferences;
+    private LocalDateTime nascimento;
+    private String photo;
+    private String cep;
+    private String endereco;
+    private String estado;
+    private String cidade;
 
 }
