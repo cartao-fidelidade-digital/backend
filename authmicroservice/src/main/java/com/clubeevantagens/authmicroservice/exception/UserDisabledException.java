38718d47805bc16d60ed51e7233d79d9d521e6d4
Gabriel Fagundes <gafagundesmesquita@gmail.com>
2025-03-22 12:59:12 -0300

    retirando classes residuais

--- authmicroservice/src/main/java/com/clubeevantagens/authmicroservice/exception/UserDisabledException.java
+++
@@ -1,9 +0,0 @@
-package com.clubeevantagens.authmicroservice.exception;
-
-public class UserDisabledException extends RuntimeException{
-
-    public UserDisabledException(String message){
-        super(message);
-    }
-
-}
--- authmicroservice/src/main/java/com/clubeevantagens/authmicroservice/repository/ReviewsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.clubeevantagens.authmicroservice.repository;
-
-import com.clubeevantagens.authmicroservice.model.Reviews;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ReviewsRepository extends MongoRepository<Reviews, Long> {
-}
